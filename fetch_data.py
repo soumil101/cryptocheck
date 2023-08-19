755905c4b9e9a39df348039afda8e26276980c66
Soumil <soumilgad@gmail.com>
2023-08-19 03:33:40 -0700

    logging

--- fetch_data.py
+++ fetch_data.py
@@ -3,11 +3,16 @@ import requests
 import json
 import datetime
 import pandas as pd
+import logging
 
 def get_current_currencies():
-    url = "https://api.coinbase.com/v2/exchange-rates?currency=USD"
-    response = requests.get(url)
-    currencies = json.loads(response.text)["data"]["rates"]
+    try:
+        url = "https://api.coinbase.com/v2/exchange-rates?currency=USD"
+        response = requests.get(url)
+        currencies = json.loads(response.text)["data"]["rates"]
+    except:
+        logging.error("Error getting current currencies from Coinbase API")
+        return
 
     # Convert exchange rates to USD
     for currency in currencies:
@@ -27,11 +32,17 @@ def get_current_currencies():
     # Convert to Pandas df
     df = pd.DataFrame.from_dict(data)
 
-    # Write to CSV
-    if os.path.getsize('./data/data.csv') == 0:
-        df.to_csv('./data/data.csv', index=False)        
-    else:
-        df.to_csv('./data/data.csv', mode='a', index=False, header=False)
+    try:
+        # Write to CSV
+        if os.path.getsize('./data/data.csv') == 0:
+            df.to_csv('./data/data.csv', index=False)        
+        else:
+            df.to_csv('./data/data.csv', mode='a', index=False, header=False)
+
+        logging.info(f"Data written to CSV at {datetime.datetime.now()}")
+    
+    except Exception as e:
+        logging.error(f"Error writing data to CSV: {e}")
 
 
 if __name__ == "__main__":
